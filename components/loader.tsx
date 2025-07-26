5964c51594e324b2cc65d0a1f98176573d7c06cf
Bohdan Hora <bohdan.hora.developer@gmail.com>
2025-07-26 06:10:40 +0300

    simplify loader

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,38 +1,9 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
-import { createPortal } from "react-dom";
-
 export const Loader = () => {
-    const [mounted, setMounted] = useState(false);
-    const elRef = useRef<HTMLDivElement | null>(null);
-
-    if (!elRef.current && typeof window !== "undefined") {
-        elRef.current = document.createElement("div");
-        elRef.current.setAttribute("id", "global-loader");
-    }
-
-    useEffect(() => {
-        setMounted(true);
-
-        const loaderEl = elRef.current!;
-        document.body.appendChild(loaderEl);
-
-        const originalOverflow = document.body.style.overflow;
-        document.body.style.overflow = "hidden";
-
-        return () => {
-            document.body.removeChild(loaderEl);
-            document.body.style.overflow = originalOverflow;
-        };
-    }, []);
-
-    if (!mounted || !elRef.current) return null;
-
-    return createPortal(
-        <div className="fixed top-0 left-0 z-[9999] w-full h-screen flex justify-center items-center bg-white dark:bg-black">
-            <div className="loader border-t-2 rounded-full border-gray-500 bg-gray-300 animate-spin aspect-square w-8 flex justify-center items-center" />
-        </div>,
-        elRef.current,
+    return (
+        <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black/30 backdrop-blur-sm">
+            <div className="w-8 h-8 border-4 border-t-transparent border-white rounded-full animate-spin" />
+        </div>
     );
 };
